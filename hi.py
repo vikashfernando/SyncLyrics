fc704440436328fcf741d76fa7bf68f8b14c7cea
vikashfernando <vikashdeshitha@gmail.com>
2026-02-11 06:30:42 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,6 +1,6 @@
 import time
 
-# Lyrics with pauses (seconds)
+
 lyrics = [
     ("Hamuwuwado Lyrics 🎵:", 1),
     ("Sonduree Hadak Nathi Kaluwara Yameee", 2),
@@ -9,15 +9,14 @@ lyrics = [
     ("Huru Na Hithath Hri Kalabala Wagee........", 2)
 ]
 
-# Function to print letters slowly
+
 def print_letter_by_letter(text, speed=0.09): 
     for letter in text:
         print(letter, end="", flush=True)
         time.sleep(speed)
-    print()  # new line after line
+    print()  
 
-# Print lyrics
+
 for line, pause in lyrics:
     print_letter_by_letter(line)
-    time.sleep(pause)  # pause after each line
-    
+    time.sleep(pause)  
